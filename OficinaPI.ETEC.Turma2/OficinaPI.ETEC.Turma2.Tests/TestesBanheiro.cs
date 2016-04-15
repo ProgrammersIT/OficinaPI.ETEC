e91c7e7688ca6c9a7515edfdd85f292fcf13f39f
Deividi Paganini <deividi.paganini@hotmail.com>
2016-04-15 09:10:54 -0300

    initial

---
+++ OficinaPI.ETEC.Turma2/OficinaPI.ETEC.Turma2.Tests/TestesBanheiro.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace OficinaPI.ETEC.Turma2.Tests
+{
+
+	public class TestesBanheiro
+	{
+		[Test]
+		public void TestaTemUmMictorioQueEstaVazio(){
+			
+			var banheiro = new Banheiro();
+			var mictorio = new Mictorio{
+				Ocupado = false
+			};
+			
+			banheiro.AdicionarMictorio(mictorio);
+						
+			var pessoas = banheiro.QuantasPessoasPodemEntrar();
+			
+			Assert.AreEqual(1 , pessoas);			
+		}
+		
+		[Test]
+		public void TestaTemUmMictorioQueEstaOcupado(){
+			
+			var banheiro = new Banheiro();
+			var mictorio = new Mictorio{
+				Ocupado = true
+			};
+			
+			banheiro.AdicionarMictorio(mictorio);
+						
+			var pessoas = banheiro.QuantasPessoasPodemEntrar();
+			
+			Assert.AreEqual(0 , pessoas);			
+		}
+		
+		[Test]
+		public void TestaTemDoisMictoriosQueEstamVazios()
+		{
+			var banheiro = new Banheiro();
+			
+			var mictorio1 = new Mictorio{
+				Ocupado = false
+			};
+			
+			var mictorio2 = new Mictorio{
+				Ocupado = false
+			};
+			
+			banheiro.AdicionarMictorio(mictorio1);
+			banheiro.AdicionarMictorio(mictorio2);
+			var pessoas = banheiro.QuantasPessoasPodemEntrar();
+			
+			Assert.AreEqual(1 , pessoas);
+		}
+		
+		[Test]
+		public void TestaTemDoisMictoriosCom_O_PrimeiroOcupado_E_O_SegundoVazio()
+		{
+			var banheiro = new Banheiro();
+			
+			var mictorio1 = new Mictorio{
+				Ocupado = true
+			};
+			
+			var mictorio2 = new Mictorio{
+				Ocupado = false
+			};
+			
+			banheiro.AdicionarMictorio(mictorio1);
+			banheiro.AdicionarMictorio(mictorio2);
+			var pessoas = banheiro.QuantasPessoasPodemEntrar();
+			
+			Assert.AreEqual(0 , pessoas);
+		}
+		
+		[Test]
+		public void TestaTemDoisMictoriosCom_O_PrimeiroVazio_E_O_SegundoOcupado()
+		{
+			var banheiro = new Banheiro();
+			
+			var mictorio1 = new Mictorio{
+				Ocupado = false
+			};
+			
+			var mictorio2 = new Mictorio{
+				Ocupado = true
+			};
+			
+			banheiro.AdicionarMictorio(mictorio1);
+			banheiro.AdicionarMictorio(mictorio2);
+			var pessoas = banheiro.QuantasPessoasPodemEntrar();
+			
+			Assert.AreEqual(0 , pessoas);
+		}
+		
+		[Test]
+		public void TestaTemDoisMictoriosComOsDoisMictoriosOcupado()
+		{
+			var banheiro = new Banheiro();
+			
+			var mictorio1 = new Mictorio{
+				Ocupado = true
+			};
+			
+			var mictorio2 = new Mictorio{
+				Ocupado = true
+			};
+			
+			banheiro.AdicionarMictorio(mictorio1);
+			banheiro.AdicionarMictorio(mictorio2);
+			var pessoas = banheiro.QuantasPessoasPodemEntrar();
+			
+			Assert.AreEqual(0 , pessoas);
+		}
+		
+		[Test]
+		public void TestaTemTresMictoriosComODoMeioOcupado()
+		{
+			var banheiro = new Banheiro();
+			
+			var mictorio1 = new Mictorio{
+				Ocupado = false
+			};
+			
+			var mictorio2 = new Mictorio{
+				Ocupado = true
+			};
+			
+			var mictorio3 = new Mictorio{
+				Ocupado = false
+			};			
+			
+			banheiro.AdicionarMictorio(mictorio1);
+			banheiro.AdicionarMictorio(mictorio2);
+			banheiro.AdicionarMictorio(mictorio3);
+			var pessoas = banheiro.QuantasPessoasPodemEntrar();
+			
+			Assert.AreEqual(0 , pessoas);
+		}	
+
+		[Test]
+		public void TestaTemTresMictorios_E_So_O_PrimeiroOcupado()
+		{
+			var banheiro = new Banheiro();
+			
+			var mictorio1 = new Mictorio{
+				Ocupado = true
+			};
+			
+			var mictorio2 = new Mictorio{
+				Ocupado = false
+			};
+			
+			var mictorio3 = new Mictorio{
+				Ocupado = false
+			};			
+			
+			banheiro.AdicionarMictorio(mictorio1);
+			banheiro.AdicionarMictorio(mictorio2);
+			banheiro.AdicionarMictorio(mictorio3);
+			var pessoas = banheiro.QuantasPessoasPodemEntrar();
+			
+			Assert.AreEqual(1 , pessoas);
+		}
+		
+		[Test]
+		public void TestaBanheiroComSeisMictoriosComSegundo_E_O_QuartoOcupado(){
+			var banheiro = new Banheiro();
+			
+			var mictorio1 = new Mictorio{ Ocupado = false };
+			var mictorio2 = new Mictorio{ Ocupado = true };
+			var mictorio3 = new Mictorio{ Ocupado = false };
+			var mictorio4 = new Mictorio{ Ocupado = true };
+			var mictorio5 = new Mictorio{ Ocupado = false };
+			var mictorio6 = new Mictorio{ Ocupado = false };
+			
+			banheiro.AdicionarMictorio(mictorio1);
+			banheiro.AdicionarMictorio(mictorio2);
+			banheiro.AdicionarMictorio(mictorio3);
+			banheiro.AdicionarMictorio(mictorio4);
+			banheiro.AdicionarMictorio(mictorio5);
+			banheiro.AdicionarMictorio(mictorio6);
+			
+			var pessoas = banheiro.QuantasPessoasPodemEntrar();
+			
+			Assert.AreEqual(1 , pessoas);
+		}
+	}
+}
---
+++ OficinaPI.ETEC.Turma2/OficinaPI.ETEC.Turma2/Banheiro.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OficinaPI.ETEC.Turma2
+{
+	public class Banheiro
+	{
+		List<Mictorio> mictorios=new List<Mictorio>();
+		
+		public void AdicionarMictorio(Mictorio novoMictorio){
+			mictorios.Add(novoMictorio);
+		}
+		
+		public int QuantasPessoasPodemEntrar(){
+			
+			if(mictorios.All(mictorio => mictorio.Ocupado))
+			   return 0;
+			
+			if(mictorios.All(mictorio => !mictorio.Ocupado))
+			   return mictorios.Count / 2 + mictorios.Count % 2;
+			
+			var total = mictorios.Count;
+			var i = 0;
+			var pessoas = 0;
+			
+			do{
+				if(mictorios[i].Ocupado) {
+					if (pessoas > 0)
+						pessoas --;
+					
+					i++;
+				}
+				else pessoas ++; 
+				
+				i++;
+			}while(i < total);
+			
+			return pessoas; 
+		}
+	}
+}
